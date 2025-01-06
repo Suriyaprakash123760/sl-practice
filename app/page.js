5d17132aa334fcd52e2a8ee857c5977ef2bbfe76
suryaprakash1085 <prakashsuriya3257@gmail.com>
2025-01-06 10:13:45 +0530

    hello

---
+++ app/page.js
@@ -0,0 +1,18 @@
+// import Image from "next/image";
+
+export default function Home() {
+  return (
+  <>
+  <div className="box">
+
+  <h1>Login</h1>
+
+<label>Gmail</label>
+<input type="text" placeholder="Enter then Email"/>
+<label>Password</label>
+<input type="text" placeholder="Enter then Password"/>
+  </div>
+  
+  </>
+  );
+}
